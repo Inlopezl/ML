3645592ef99671754f20f886af6bd4b249ac7690
Inlopezl <ivanxlopez@gmail.com>
2021-06-30 20:01:42 -0300

    ML
    Arregladas media queries y Href

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require("path")
 
 //server
-app.listen( process.env.PORT || 3030, ()=> console.log("Server on http://localhost:3000"));
+app.listen(process.env.PORT ||   3000, ()=> console.log("Server on http://localhost:3000"));
 
 // Public Access
 app.use(express.static(path.resolve(__dirname,"../public")));
